4d75bfa7d2b480a8183362a6f82f68db797e7b2d
Adrian Salceanu <adrian.salceanu@gmail.com>
2022-09-20 11:44:25 +0200

    initial commit

--- src/GenieFramework.jl
+++ src/GenieFramework.jl
@@ -1,5 +1,36 @@
 module GenieFramework
 
-# Write your package code here.
+using Reexport
+
+@reexport using Genie
+@reexport using Stipple
+@reexport using StippleUI
+@reexport using StipplePlotly
+
+@reexport using Stipple.Pages
+@reexport using Stipple.ModelStorage.Sessions
+@reexport using Stipple.ReactiveTools
+
+export @devtools
+
+macro devtools()
+  Genie.Configuration.isdev() || return :(nothing)
+
+  quote
+    using GenieDevTools
+    using GenieAutoReload
+
+    Genie.Logger.initialize_logging()
+
+    GenieDevTools.register_routes()
+    Stipple.deps!(GenieAutoReload, GenieAutoReload.deps)
+    autoreload(pwd())
+
+    @async begin
+      sleep(2)
+      Genie.Watch.watch()
+    end
+  end |> esc
+end
 
 end
