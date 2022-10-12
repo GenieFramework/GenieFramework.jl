9b95dde536e95fcea3218a0adf24ad6a26ec7ef4
Adrian Salceanu <adrian.salceanu@gmail.com>
2022-10-12 10:29:24 +0200

    Mock integration with GeniePackageManager, explicit restrict to dev

--- src/GenieFramework.jl
+++ src/GenieFramework.jl
@@ -21,6 +21,7 @@ if Genie.Configuration.isdev()
   @reexport using GenieDevTools
   @reexport using GenieAutoReload
   @reexport using GarishPrint
+  # @reexport using GeniePackageManager
 end
 
 macro genietools()
@@ -31,14 +32,18 @@ macro genietools()
   end
 
   quote
-    Genie.Logger.initialize_logging()
-    GenieDevTools.register_routes()
-    Stipple.deps!(GenieAutoReload, GenieAutoReload.deps)
-    @async begin
-      autoreload(pwd())
-      sleep(2)
-      Genie.Watch.watch()
+    if Genie.Configuration.isdev()
+      Genie.Logger.initialize_logging()
+      GenieDevTools.register_routes()
+      # GeniePackageManager.register_routes()
+      Stipple.deps!(GenieAutoReload, GenieAutoReload.deps)
+      @async begin
+        autoreload(pwd())
+        sleep(2)
+        Genie.Watch.watch()
+      end
     end
+
     nothing
   end |> esc
 end
